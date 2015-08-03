840e393776566a458057d40880c8b105b3ad5e96
Mads Kristensen <post@madskristensen.net>
2015-08-03 13:54:54 -0700

    Removed JSON.NET depedency

--- src/TaskParser.cs
+++ src/TaskParser.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.IO;
-using Newtonsoft.Json.Linq;
 
 namespace BroccoliTaskRunner
 {
@@ -15,29 +13,5 @@ namespace BroccoliTaskRunner
 
             return dic;
         }
-
-        //public static Dictionary<string, string> LoadTasks(string configPath)
-        //{
-        //    Dictionary<string, string> dic = new Dictionary<string, string>();
-
-        //    try {
-        //        string document = File.ReadAllText(configPath);
-        //        JObject root = JObject.Parse(document);
-
-        //        var children = root["scripts"].Children<JProperty>();
-
-        //        foreach (var child in children)
-        //        {
-        //            dic.Add(child.Name, child.Value.ToString());
-        //        }
-        //    }
-        //    catch
-        //    {
-        //        return null;
-        //        // TODO: Implement logger
-        //    }
-
-        //    return dic;
-        //}
     }
 }
